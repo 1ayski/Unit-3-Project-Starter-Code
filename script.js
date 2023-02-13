533ef9074d33e03c10cee11537f16f0d7bcb6dbe
Amari Clark <125311175+1ayski@users.noreply.github.com>
2023-02-13 14:32:26 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,30 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let bulls=document.querySelector(".option-one");
+let hornets=document.querySelector(".option-two");
+let bullsScreen= document.querySelector(".option-one-screen");
+let hornetsScreen= document.querySelector(".option-two-screen");
+let bullsEnd= document.querySelector(".option-one-end");
+let hornetsEnd= document.querySelector(".option-two-end");
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
